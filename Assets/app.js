75c4d103641cb0e34e9ec17e9bfcbfd7cdbd20f2
Hudson Hancock <hudsonhancock@gmail.com>
2021-07-27 17:08:53 -0400

    new cards and button to go to top

--- Assets/app.js
+++ Assets/app.js
@@ -33,20 +33,45 @@ const countdown = () => {
 
 setInterval(countdown, 1000);
 
-// When the user scrolls the page, execute myFunction
-window.onscroll = function() {stickyNav()};
+let mybutton = document.getElementById("btn-back-to-top");
 
-// Get the navbar
-var navbar = document.getElementById('navbar');
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function () {
+  scrollFunction();
+};
 
-// Get the offset position of the navbar
-var sticky = navbar.offsetTop;
-
-// Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
-function stickyNav() {
-  if (window.pageYOffset >= sticky) {
-    navbar.classList.add("sticky")
+function scrollFunction() {
+  if (
+    document.body.scrollTop > 20 ||
+    document.documentElement.scrollTop > 20
+  ) {
+    mybutton.style.display = "block";
   } else {
-    navbar.classList.remove("sticky");
+    mybutton.style.display = "none";
   }
 }
+// When the user clicks on the button, scroll to the top of the document
+mybutton.addEventListener("click", backToTop);
+
+function backToTop() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
+
+// When the user scrolls the page, execute myFunction
+// window.onscroll = function() {stickyNav()};
+
+// Get the navbar
+// var navbar = document.getElementById('navbar');
+
+// Get the offset position of the navbar
+// var sticky = navbar.offsetTop;
+
+// Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
+// function stickyNav() {
+//   if (window.pageYOffset >= sticky) {
+//     navbar.classList.add("sticky")
+//   } else {
+//     navbar.classList.remove("sticky");
+//   }
+// }
